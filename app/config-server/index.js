087820e7e640959e67feb285699f1205bc16b3d2
Julian Correa <julian@torre.co>
2018-04-26 21:13:29 -0500

    front static files

--- app/config-server/index.js
+++ app/config-server/index.js
@@ -11,4 +11,5 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require('./app/config-server/');
+const express = require('express');
+const path = require('path');
 require('./app/routes/');
 
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`server started at ${port}`);
 });
