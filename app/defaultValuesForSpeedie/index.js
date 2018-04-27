e08dd8dc9a4a1ae0dd62b7a8e36d29684a530073
Julian Correa <julian@torre.co>
2018-04-26 20:54:33 -0500

    final route

---
+++ app/defaultValuesForSpeedie/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  genderAndAge: 'middleAgeMale',
+  language: 'eng-us',
+  fake_project: 1,
+  remarks: 'This is a project to record movies overviews use a movie trailer kind of voice',
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,13 @@
 const app = require('../config-server/');
 const setGetCall = require('../getApi/');
 const setPostCall = require('../postApi/');
+const defaultValuesForSpeedie = require('../defaultValuesForSpeedie/');
 
 const userId = process.env.USER_ID;
 const token = process.env.TOKEN;
 const apiKey = process.env.API_KEY;
 const getMovie = setGetCall('https://api.themoviedb.org/3/search/multi');
-const postProject = setPostCall(`https://${userId}:${token}@api.voicebunny.com/projects`);
+const postProject = setPostCall(`https://${userId}:${token}@api.voicebunny.com/projects/addSpeedy`);
 
 const params = {
   api_key: apiKey,
@@ -22,7 +23,26 @@ app.get('/movies', (req, res) => {
 
 app.post('/projects', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  postProject()
-    .then(response => res.send(JSON.stringify(response.data.results)))
-    .catch(error => res.send(JSON.stringify(error)));
+  const obj = Object.assign({}, defaultValuesForSpeedie, {
+    script: req.body.overview,
+    title: `[test] #noflags ${req.body.title}`,
+  });
+  postProject(obj)
+    .then((response) => {
+      res.send(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      res.send(JSON.stringify({ error: error.message, status: error.status }));
+    });
+});
+
+app.get('/projects/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  setGetCall(`https://${userId}:${token}@api.voicebunny.com/projects/${req.params.id}`)()
+    .then((response) => {
+      res.send(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      res.send(JSON.stringify({ error: error.message, status: error.status }));
+    });
 });
